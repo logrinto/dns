410c75b33a3c1fa5b2804b0a4b461b80effc79c4
signalwerk <sh@signalwerk.ch>
2020-05-07 02:02:55 +0200

    MOD: fix env

--- run.sh
+++ run.sh
@@ -3,8 +3,7 @@
 echo "ℹ️  DNSControl check"
 
 docker run --rm -it \
--e CLOUDFLARE_EMAIL="$CLOUDFLARE_EMAIL" \
--e CLOUDFLARE_API_KEY="$CLOUDFLARE_API_KEY" \
+-e CLOUDFLARE_API_TOKEN="$CLOUDFLARE_API_TOKEN" \
 -v $(pwd)/:/dns/ stackexchange/dnscontrol \
 dnscontrol check
 
@@ -12,16 +11,14 @@ if [[ $TRAVIS_BRANCH == 'master' ]]; then
   echo "ℹ️  DNSControl push"
 
   docker run --rm -it \
-  -e CLOUDFLARE_EMAIL="$CLOUDFLARE_EMAIL" \
-  -e CLOUDFLARE_API_KEY="$CLOUDFLARE_API_KEY" \
+  -e CLOUDFLARE_API_TOKEN="$CLOUDFLARE_API_TOKEN" \
   -v $(pwd)/:/dns/ stackexchange/dnscontrol \
   dnscontrol push
 else
   echo "ℹ️  DNSControl preview"
 
   docker run --rm -it \
-  -e CLOUDFLARE_EMAIL="$CLOUDFLARE_EMAIL" \
-  -e CLOUDFLARE_API_KEY="$CLOUDFLARE_API_KEY" \
+  -e CLOUDFLARE_API_TOKEN="$CLOUDFLARE_API_TOKEN" \
   -v $(pwd)/:/dns/ stackexchange/dnscontrol \
   dnscontrol preview
 
